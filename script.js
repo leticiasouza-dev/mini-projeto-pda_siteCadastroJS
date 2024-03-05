5887615bc41f89e8bd5dcbc7bc359c5ec740b1e3
leticiaSouzaDev <lec667121@gmail.com>
2024-03-05 15:38:53 -0300

    feat: adicionando a verificação de idade do usuário

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Pessoa{
+class Pessoa{ //  classe pessoa onde vai receber os valores do meu input
     constructor(nomeCompleto, celular, email, senha, dataNascimento){
         this.nomeCompleto = nomeCompleto;
         this.celular = celular;
@@ -8,14 +8,8 @@ class Pessoa{
     }
 }
 
-let usuario = new Pessoa();
-
-let botaoCadastro = document.getElementById('cadastro');
-let inputs = document.getElementsByTagName('input');
-
-botaoCadastro.addEventListener('click', function(evento){
-    evento.preventDefault();
-
+function verificacaoCampos(){ // faz a verificação dos campos(inputs)
+    let inputs = document.getElementsByTagName('input');
     let camposNaoPreenchidos = false;
 
     for(let i = 1; i < inputs.length; i++){
@@ -29,9 +23,42 @@ botaoCadastro.addEventListener('click', function(evento){
     if(camposNaoPreenchidos){
         alert("Preencha os campos");
     } else{
-        window.location.href = 'pages/listaUsuariosCadastrados.html';
-    }
+        let dataNascimento = pegarElementosId('dataNascimento').value;
 
+        if(verificarIdade(dataNascimento)){
+            alert("Você não poderá seguir com o cadastro pois é menor der idade");
+        } else{
+            window.location.href = 'pages/listaUsuariosCadastrados.html';
+        }
+        
+    }
+}
+
+function verificarIdade(dataNascimento){ // faz a verificação da idade do usuário
+    let anoAtual = new Date().getFullYear();
+    let anoNascimento = new Date(dataNascimento).getFullYear();
+    let idadeUsuario = anoAtual - anoNascimento;
     
+    return idadeUsuario < 18;
+}
+
+function pegarElementosId(nomeId){ // função para facilitar na hora de pegar elementos com Id
+    return document.getElementById(nomeId);
+}
+
+let botaoCadastro = document.getElementById('cadastro');
+
+botaoCadastro.addEventListener('click', function(evento){
+    evento.preventDefault();
+
+    let nomeCompleto = pegarElementosId('nomeCompleto').value;
+    let celular = pegarElementosId('celular').value;
+    let email = pegarElementosId('email').value;
+    let senha = pegarElementosId('senha').value;
+    let dataNascimento = pegarElementosId('dataNascimento').value;
+
+    verificacaoCampos();
+
+    let usuario = new Pessoa(nomeCompleto, celular, email, senha, dataNascimento);
 })
 
