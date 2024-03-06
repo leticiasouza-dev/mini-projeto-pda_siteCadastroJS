01ee1730aa95f6baa0a5dd396c845630570ccc19
leticiaSouzaDev <lec667121@gmail.com>
2024-03-06 15:50:44 -0300

    iniciando a funcionalidade de mostrar as informações dos cadastrados

--- pages/scriptListaUsuários.js
+++ pages/scriptListaUsuários.js
@@ -1,4 +1,32 @@
 let usuarioJson = localStorage.getItem('usuario');
 let usuario = JSON.parse(usuarioJson);
 
-console.log(usuario.celular);
+mostrarCadastro();
+
+function mostrarCadastro(){
+    const sessaoPrincipal = document.getElementById('sessao-cadastros');
+
+    let divInfosCadastrados = document.createElement('div');
+    divInfosCadastrados.className = 'informacoes-cadastrado';
+
+    for(const cadastro in usuario){ // percorrendo cada propriedade do meu array
+        const p = document.createElement('p');
+        p.innerHTML = `${usuario[cadastro]}`;
+        divInfosCadastrados.appendChild(p);
+    }
+
+    sessaoPrincipal.appendChild(divInfosCadastrados); // adicionando a minha sessão principal minha div com os paragrafos
+}
+    
+
+
+
+
+
+// let divBotoes = document.createElement('div');
+// divBotoes.className = 'botoes';
+
+// divInfosCadastrados.appendChild(divInfosCadastrados);
+// divBotoes.appendChild(divBotoes)
+
+
