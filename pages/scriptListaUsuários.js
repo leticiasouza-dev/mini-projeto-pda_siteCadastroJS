0f3157fea649bda1950b9b897fd056690b97c279
leticiaSouzaDev <lec667121@gmail.com>
2024-03-06 16:39:32 -0300

    feat:  adicionando os botoes

--- pages/scriptListaUsuários.js
+++ pages/scriptListaUsuários.js
@@ -1,15 +1,19 @@
 let cadastroJson = localStorage.getItem('cadastro');
-console.log(cadastroJson);
 let cadastro = JSON.parse(cadastroJson);
-console.log(cadastro);
+
 
 mostrarCadastro();
+
+
 
 function mostrarCadastro(){
     const sessaoPrincipal = document.getElementById('sessao-cadastros');
 
-    let divInfosCadastrados = document.createElement('div');
+    const divInfosCadastrados = document.createElement('div');
     divInfosCadastrados.className = 'informacoes-cadastrado';
+
+    const divBotoes = document.createElement('div');
+    divBotoes.className = 'botoes';
 
     for(const pessoa of cadastro){ // percorrendo meu objeto no array
         for(const propriedade in pessoa){ // percorrendo cada propriedade do meu array(objeto)
@@ -17,10 +21,20 @@ function mostrarCadastro(){
             p.innerHTML = `${pessoa[propriedade]}`;
             divInfosCadastrados.appendChild(p);
         }
-    }
-    
 
+        const botaoEditar = document.createElement('button')
+        botaoEditar.textContent = 'Editar';
+        divBotoes.appendChild(botaoEditar);
+
+        const botaoRemover = document.createElement('button');
+        botaoRemover.textContent = 'Remover';
+        divBotoes.appendChild(botaoRemover);
+    }
     sessaoPrincipal.appendChild(divInfosCadastrados); // adicionando a minha sessão principal minha div com os paragrafos
+    sessaoPrincipal.appendChild(divBotoes);
+    
+    
+    
 }
 
     
